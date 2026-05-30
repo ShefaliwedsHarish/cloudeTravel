37868c7674dbf71e7649de4a73a484db94bf4578
Hari Om <hariom1gyan@gmail.com>
2026-05-30 17:09:53 +0530

    Update the app file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,7 +42,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
 
         @viteReactRefresh
-        <!-- @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"]) -->
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
             <title>{{ config('app.name', 'Laravel') }}</title>
